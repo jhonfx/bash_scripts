49af66c93e96505b7da84d08da53742e99c66c99
juan purata <juan.purata@twinkey.cool>
2016-03-03 15:48:56 -0600

    update restore script and generate new showdb script

--- restoreDBS.sh
+++ restoreDBS.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+red='\E[31;47m'
+green='\E[32;47m'
+yellow='\E[33;47m'
+blue='\E[34;47m'
+magenta='\E[35;47m'
+cyan='\E[36;47m'
+white='\E[37;47m'
 # Config Variables:
 USER="root"
 HOST="localhost"
@@ -32,24 +40,59 @@ if [ "$(ls -A *.sql 2> /dev/null)" == 0 ]; then
 fi
 COMMENT
 # Get all database list first
-DBS="$(mysql -u $USER -h $HOST -p$PASS -Bse 'show databases')"
+DBS="$(mysql -u $USER -h $HOST -Bse 'show databases')"
 
-echo "These are the current existing Databases:"
-echo $DBS
+echo "THESE ARE THE CURRENT EXISTING DATABASES:"
+echo "________________________________________"
+echo "$DBS"
+echo "________________________________________"
 
 # Ignore list, won't restore the following list of DB:
 IGGY="test information_schema mysql"
-:<<'COMMENT'
-COMMENT
 # Restore DBs:
-echo 'Databases for insert'
+echo 'DATABASES FOR INSERT'
 for filename in *.sql
 do
   dbname=${filename%.sql}
-  echo $dbname
-    echo "making new databases"
-    mysql -u root -p -e "create database $dbname"
+  skipdb=-1
+  echo "[$dbname]"
 
-    echo "Importing DB: $dbname from $filename"
-    mysql $dbname < $filename -u $USER -p$PASS
+  if [ "$IGGY" != "" ]; then
+    for ignore in $IGGY
+    do
+        [ "$dbname" == "$ignore" ] && skipdb=1 || :
+    done
+  fi
+done
+
+echo "__________________________________________"
+echo "CHECKING IF EXIST ANY DATABASE OF THE LIST"
+for filename in *.sql 
+do
+  dbname=${filename%.sql}
+  skipdb=-1
+  #evaluate the dblist
+  if [ "$skipdb" == "-1" ] ; then
+  
+    skip_create=-1
+    for existing in $DBS
+    do      
+      #echo "Checking database: $dbname to $existing"
+      [ "$dbname" == "$existing" ] && skip_create=1 || :
+    done
+    
+    if [ "$skip_create" ==  "1" ] ; then
+      printf "DATABASE \033[1;31m$dbname\033[0m ALREADY EXIST, SKIPING CREATE\n"
+    else
+      echo "____________________________________________"
+      echo "CREATE NEW DATABASES"
+      printf "\033[1;92m[$dbname]\033[0m \n"
+      
+      echo "============================================"
+      mysql -u root -e "create database $dbname"
+
+      printf "IMPORTING DB: \033[1;92m[$dbname]\033[0m from $filename\n"
+      mysql $dbname < $filename -u $USER
+    fi
+  fi
 done
